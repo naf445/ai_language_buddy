51f98f466060dc7e454d5bc7d26a776f9d2c61b8
Nathan Franklin <nathanfranklin2019@u.northwestern.edu>
2024-08-30 14:45:29 -0500

    add chat history, actual open ai call

--- app.py
+++ app.py
@@ -3,8 +3,51 @@ from faster_whisper import WhisperModel
 import edge_tts
 import tempfile
 import asyncio
+import yaml
+import os
+import openai
+
+open_ai_client = openai.OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
 model = WhisperModel("tiny", compute_type="float32")
+
+with open('config.yml', 'r') as file:
+    config = yaml.safe_load(file)
+
+def generate_prompt(personality: str, user_query: str) -> str:
+    
+    prompt = f'''
+{config['prompts']['base']}
+{config['prompts'][personality]}
+
+User query:
+
+{user_query} -> '''
+
+    return prompt
+
+
+def gpt_answer(prompt, personality, chatbot_history):
+
+    print(f'going to send the prompt: {prompt}')
+
+    history_for_gpt_call = [
+        {"role": "system", "content": f"You are a helpful assistant, with the personality of a {personality}."}
+    ] + chatbot_history + [
+        {"role": "user", "content": prompt}
+    ]
+
+    completion =  open_ai_client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=history_for_gpt_call
+    )
+
+    # Extract the generated response from the API response
+    generated_text = completion.choices[0].message.content.strip()
+
+    return generated_text
 
 # Text-to-speech function
 async def text_to_speech(text, voice):   
@@ -32,8 +75,14 @@ def generate_response(
     user_query_transcribed = list(user_query_transcribed_segments)[0].text.strip()
 
     # Ask llm for response to text
+    prompt = generate_prompt(
+        personality=buddy_personality,
+        user_query=user_query_transcribed
+    )
 
-    bot_message = user_query_transcribed
+    bot_message = gpt_answer(prompt=prompt,
+                             personality=buddy_personality,
+                             chatbot_history=chatbot_history)
 
     chatbot_history.append(gr.ChatMessage(role="user", content=user_query_transcribed))
     chatbot_history.append(gr.ChatMessage(role="assistant", content=bot_message))
